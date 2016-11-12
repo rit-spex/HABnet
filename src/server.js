89297cca20349e695f0d4c27a757c940e321b8e5
Thomas Tarazevits (RIT Student) <tarazevits@gmail.com>
2016-11-12 18:45:31 -0500

    Working Websocketsgit status!

--- src/server.js
+++ src/server.js
@@ -25,19 +25,36 @@ const onJoined = (sock) => {
   const socket = sock;
 
   socket.on('join', (data) => {
-    console.log('New Connection')
+    console.log(`New Connection: ${ data.name }`)
     if (data.type == 'dataSource') {
       dataSources[data.name] = '';
     } else if (data.type == 'dataListener') {
       dataListeners[data.name] = '';
     }
+
+    socket.name = data.name;
+    socket.join('room1');
   });
 
   socket.on('sensorData', (data) => {
     socket.broadcast.to('room1').emit('broadcastData', data);
     console.log('broadcasted data');
   });
-
-  socket.name = data.name;
-  socket.join('room1');
 };
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+  socket.on('disconnect', (data) => {
+    //delete users[socket.name];
+    //io.sockets.in('room1').emit('msg', { name: 'server', msg: `${socket.name} has disconnected` });
+    console.log(data);
+  });
+};
+
+io.sockets.on('connection', (socket) => {
+  console.log('started');
+
+  onJoined(socket);
+});
+
+console.log(`Listening on 127.0.0.1: ${port}`);
