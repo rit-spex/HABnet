f0bf671d654da49755f33e53f37848e622b813e2
T.J. Tarazevits <tarazevits@gmail.com>
2017-04-23 23:06:15 -0400

    Added barebones socket manager

---
+++ client/components/SocketManager.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SocketManager = React.createClass({
+  render() {
+    return(<h2>This component will handle socket initializtion and connecting to data sources </h2>);
+  }
+});
+
+export default SocketManager;
